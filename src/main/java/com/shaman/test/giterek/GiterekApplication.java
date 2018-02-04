88561980b86bd6cbeb45c848dc43feb7238ab967
Michal Pieprzyca <michal.pieprzyca@soprasteria.com>
2018-02-05 00:11:03 +0100

    Base version with no DB type configured

---
+++ src/main/java/com/shaman/test/giterek/GiterekApplication.java
@@ -0,0 +1,12 @@
+package com.shaman.test.giterek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiterekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiterekApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shaman/test/giterek/GiterekApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shaman.test.giterek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiterekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
