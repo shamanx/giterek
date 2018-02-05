5c1a129f535f78a074bca060c7dbd0cb116a0a61
Michal Pieprzyca <michal.pieprzyca@soprasteria.com>
2018-02-05 01:28:54 +0100

    add POLAND to ProductionSites

--- src/main/java/com/shaman/test/giterek/configuration/RoutingDataSource.java
+++ src/main/java/com/shaman/test/giterek/configuration/RoutingDataSource.java
@@ -4,8 +4,11 @@ package com.shaman.test.giterek.configuration;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 public class RoutingDataSource extends AbstractRoutingDataSource {
+	
     @Override
     protected Object determineCurrentLookupKey() {
         return DataSourceContext.getCurrentDB();
     }
+    
+    
 }
